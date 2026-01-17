5ad9497f5271b1f3f831d22093373bfe77d5f204
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2026-01-17 13:06:40 +0530

    added some fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -22,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-app.get("*", (req, res) => {
+app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 module.exports = app;
